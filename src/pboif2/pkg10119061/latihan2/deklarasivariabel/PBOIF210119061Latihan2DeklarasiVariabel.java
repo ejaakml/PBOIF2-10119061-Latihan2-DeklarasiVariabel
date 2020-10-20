d9f2a0806cad33017d21a718117ef6aa0a99bb70
ejaakml <eximindotong@yahoo.com>
2020-10-20 17:02:31 +0700

    q

---
+++ src/pboif2/pkg10119061/latihan2/deklarasivariabel/PBOIF210119061Latihan2DeklarasiVariabel.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pboif2.pkg10119061.latihan2.deklarasivariabel;
+
+/**
+ *
+ * @author Rheiza
+ */
+public class PBOIF210119061Latihan2DeklarasiVariabel {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // DeklarasiVariabel
+        int nilaiInt;
+        final double PHI = 2.14; //konstanta uppercase
+        boolean nilaiLogika;
+        char nilaiKarakter;
+        
+        // Memberi nilai ke variabel
+        nilaiInt = 78;
+        nilaiLogika = false;
+        nilaiKarakter='D';
+        
+        // Menampilkan hasil
+        System.out.println();
+        System.out.println("Isi variabel nilai = " + nilaiInt);
+        System.out.println("ISi variabel PHI = " + PHI);
+        System.out.println("Isi variabel logika = " + nilaiLogika);
+        System.out.println("Isi vairabel Karkater = " + nilaiKarakter);
+        
+        
+    }
+    
+}
